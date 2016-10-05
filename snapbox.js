db5580ca98d52536dfaaf96473ee91fc1c81fb20
Kim Spasaro <spasarok@grinnell.edu>
2016-10-05 14:42:38 -0500

    changing naming convention

--- snapbox.js
+++ snapbox.js
@@ -16,15 +16,15 @@ jQuery(document).ready( function($) {
     };
 
     /*
-     * Controller prototype
-     * Aggregates model view for a single gallery
+     * Gallery prototype
+     * Create view for specified gallery element
      */
 
-    function Controller(gallery) {
+    function Gallery(gallery) {
         this.gallery = gallery;
         this.galleryID = Math.floor(Math.random() * 100000);
         this.gallery.addClass('snapbox-gallery-' + this.galleryID);
-        this.model = null;
+        this.state = null;
         this.view = new View(this);
     }
 
@@ -33,11 +33,11 @@ jQuery(document).ready( function($) {
      * Keeps track of lightbox elements
      */
 
-    function View(controller) {
+    function View(gallery) {
 
         // Set lightbox values
-        this.controller = controller;
-        this.el = $(document.createElement('div')).addClass('snapbox-lightbox').addClass('snapbox-lightbox-' + this.controller.galleryID);
+        this.gallery = gallery;
+        this.el = $(document.createElement('div')).addClass('snapbox-lightbox').addClass('snapbox-lightbox-' + this.gallery.galleryID);
         this.nextButton = $(document.createElement('div')).addClass('snapbox-control-toggle-next');
         this.prevButton = $(document.createElement('div')).addClass('snapbox-control-toggle-prev');
         this.closeButton = $(document.createElement('div')).addClass('snapbox-control-close');
@@ -46,9 +46,9 @@ jQuery(document).ready( function($) {
         this.visible = false;
 
         // Scope variables
-        this.controller.model = new Model(this.controller);
-        var model = this.controller.model;
-        var ctrl = this.controller;
+        this.gallery.state = new State(this.gallery);
+        var state = this.gallery.state;
+        var ctrl = this.gallery;
         var view = this;
         var el = this.el;
         var nextButton = this.nextButton;
@@ -182,7 +182,7 @@ jQuery(document).ready( function($) {
      * Stores gallery image data
      */
 
-    function Image(imageID, controller) {
+    function Image(imageID, gallery) {
         // Set values
         this.id = imageID;
         this.image = null;
@@ -190,19 +190,19 @@ jQuery(document).ready( function($) {
         this.next = null;
         this.loaded = false;
         this.preloaded = false;
-        this.controller = controller;
+        this.gallery = gallery;
 
         // Scope variables
         var imObj = this;
-        var controller = this.controller;
+        var gallery = this.gallery;
 
         // Add thumbnail click event
-        controller.gallery.find('li.snapbox-' + imageID + ' .' + pre.thumbID).click(function(){
-            controller.view.showLightbox(imObj);
+        gallery.gallery.find('li.snapbox-' + imageID + ' .' + pre.thumbID).click(function(){
+            gallery.view.showLightbox(imObj);
         });
 
         this.preload = function() {
-            var view = controller.view;
+            var view = gallery.view;
 
             if(imObj.next != null && !imObj.next.loaded){
                 imObj.next.loaded = true;
@@ -251,30 +251,30 @@ jQuery(document).ready( function($) {
     }
 
     /*
-     * Model Prototype
-     * Stores lightbox data
+     * State Prototype
+     * Stores lightbox state data
      */
 
-    function Model(controller) {
-        this.controller = controller;
+    function State(gallery) {
+        this.gallery = gallery;
         this.images = {};
         this.lastPushed = null;   // Last image pushed to the image array
         this.first = null;        // True if at the first image
         this.last = null;       // True if at the last image
 
         /*
-         * Model Functions
+         * State Functions
          */
 
         // Set the array of lightbox images
         this.setImages = function(){
-            var gallery = this.controller.gallery;
+            var gallery = this.gallery.gallery;
             var thumbs = gallery.find('li');
 
             for(var n = 0; n < thumbs.length; n++){
                 var thumb = $(thumbs[n]);
                 var imageID = thumb.attr('data-snapbox-id');
-                var image = new Image(imageID, this.controller);
+                var image = new Image(imageID, this.gallery);
                 this.pushImage(image);
             }
         };
@@ -301,7 +301,7 @@ jQuery(document).ready( function($) {
     function main(){
         var galleries = $('.snapbox-gallery');
         for(var i = 0; i < galleries.length; i++){
-            new Controller($(galleries[i]));
+            new Gallery($(galleries[i]));
         }
     }
 
